25798dddf67ca2e2c17f74df41e7a6d0f7ed465b
Lukas-ren <lu.duranh@duocuc.cl>
2026-01-23 22:25:19 -0300

    Pantalla de registro completada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3)
     implementation(libs.compose.material3)
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/closetfit/view/RegistroScreen.kt
+++ app/src/main/java/com/example/closetfit/view/RegistroScreen.kt
@@ -1,44 +1,25 @@
 package com.example.closetfit.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.closetfit.ui.theme.colorPrimario
 import com.example.closetfit.viewmodel.AuthViewModel
-import androidx.compose.ui.graphics.Color
-import androidx.wear.compose.material3.TextButtonDefaults
-
 
 @Composable
 fun RegistroScreen(navController: NavController, viewModel: AuthViewModel) {
-    // Estados de los campos
-    var run by remember { mutableStateOf("") }
     var nombre by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var run by remember { mutableStateOf("") }
     var direccion by remember { mutableStateOf("") }
 
     Surface(
@@ -46,40 +27,20 @@ fun RegistroScreen(navController: NavController, viewModel: AuthViewModel) {
         color = colorPrimario,
         contentColor = Color.White
     ) {
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp),
-            verticalArrangement = Arrangement.Center
+                .padding(20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            Text(
-                text = "Registro de Cuenta",
-                style = MaterialTheme.typography.headlineMedium
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            OutlinedTextField(
-                value = run,
-                onValueChange = { run = it },
-                label = { Text("RUT") },
-                modifier = Modifier.fillMaxWidth(),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Color.White,
-                    unfocusedBorderColor = Color.White,
-                    focusedLabelColor = Color.White,
-                    unfocusedLabelColor = Color.White,
-                    focusedTextColor = Color.White,
-                    unfocusedTextColor = Color.White
-                )
-            )
+            Text("Registro", style = MaterialTheme.typography.titleLarge)
 
             OutlinedTextField(
                 value = nombre,
                 onValueChange = { nombre = it },
                 label = { Text("Nombre") },
-                modifier = Modifier.fillMaxWidth(),
                 colors = OutlinedTextFieldDefaults.colors(
                     focusedBorderColor = Color.White,
                     unfocusedBorderColor = Color.White,
@@ -94,10 +55,34 @@ fun RegistroScreen(navController: NavController, viewModel: AuthViewModel) {
                 value = email,
                 onValueChange = { email = it },
                 label = { Text("Email") },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email
-                ),
-                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.White,
+                    focusedLabelColor = Color.White,
+                    unfocusedLabelColor = Color.White,
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White
+                )
+            )
+
+            OutlinedTextField(
+                value = run,
+                onValueChange = { run = it },
+                label = { Text("RUN") },
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.White,
+                    focusedLabelColor = Color.White,
+                    unfocusedLabelColor = Color.White,
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White
+                )
+            )
+
+            OutlinedTextField(
+                value = direccion,
+                onValueChange = { direccion = it },
+                label = { Text("Dirección") },
                 colors = OutlinedTextFieldDefaults.colors(
                     focusedBorderColor = Color.White,
                     unfocusedBorderColor = Color.White,
@@ -113,10 +98,6 @@ fun RegistroScreen(navController: NavController, viewModel: AuthViewModel) {
                 onValueChange = { password = it },
                 label = { Text("Contraseña") },
                 visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password
-                ),
-                modifier = Modifier.fillMaxWidth(),
                 colors = OutlinedTextFieldDefaults.colors(
                     focusedBorderColor = Color.White,
                     unfocusedBorderColor = Color.White,
@@ -128,11 +109,10 @@ fun RegistroScreen(navController: NavController, viewModel: AuthViewModel) {
             )
 
             OutlinedTextField(
-                value = direccion,
-                onValueChange = { direccion = it },
-                label = { Text("Dirección") },
-                modifier = Modifier
-                    .fillMaxWidth(),
+                value = confirmPassword,
+                onValueChange = {confirmPassword = it},
+                label = {Text("Confirme contraseña")},
+                visualTransformation = PasswordVisualTransformation(),
                 colors = OutlinedTextFieldDefaults.colors(
                     focusedBorderColor = Color.White,
                     unfocusedBorderColor = Color.White,
@@ -143,35 +123,21 @@ fun RegistroScreen(navController: NavController, viewModel: AuthViewModel) {
                 )
             )
 
-            Spacer(modifier = Modifier.height(24.dp))
-            Button(
-                onClick = {
-                    // Acción al registrar
-                    println("RUN: $run")
-                    println("Nombre: $nombre")
-                    println("Email: $email")
-                    println("Password: $password")
-                    println("Dirección: $direccion")
-                },
-                modifier = Modifier.fillMaxWidth(),
-                colors = androidx.compose.material3.ButtonDefaults.buttonColors(
-                    containerColor = Color.White,
-                    contentColor = colorPrimario
-                )
-            ) {
-                Text("Registrarse")
+            Spacer(modifier = Modifier.height(20.dp)) // Un poco más de espacio antes del botón
+
+            Button(onClick = { viewModel.registrar(nombre, email, password, run, direccion) }) {
+                Text("Registrar")
             }
-            // 
 
             Text(viewModel.mensaje.value, modifier = Modifier.padding(top = 10.dp))
 
             TextButton(onClick = { navController.navigate("login") }) {
                 Text("¿Ya tienes cuenta? Inicia sesión",
                     color = Color(0xFFFFFFFF))
-                }
             }
         }
     }
+}
 
 
 
